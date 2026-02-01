b32bccfbe3b5e4aa85134a52e73883fd4ea28979
kike-algoclick <aldo.barriere2026@hilasal.superate.org>
2026-02-01 00:08:58 -0600

    navbar iniciada

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return ( 
+    <div className="w-full border-2 border-blue-200 flex justify-center">
+        <div className="Logo flex justify-center">
+                <a href="/home">
+              <img src="" alt="TeachMeNowLogo" />
+              </a>
+            
+          </div>
+    </div> )
+   
+    
+}
